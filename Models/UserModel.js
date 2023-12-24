a759cf0a3d741f384615fe15d64126cfdef1a2d1
Sameera lakshitha <123750409+LakshithaEzi@users.noreply.github.com>
2023-12-24 12:21:26 +0530

    error fixing

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,6 +1,6 @@
-var mongoose = require ("mongoose");
-var bcrypt = require ("bcrypt");
-require("dotevn").config();
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 var schema = mongoose.schema;
@@ -8,92 +8,90 @@ var schema = mongoose.schema;
 var SALT = 10;
 
 var UserSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required: [ true, "Name feild is required"],
-        maxlength: 100
-    },
-    email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: [true, "Email is already exists on our website"]
-    },
-    username :{
-        tyep:String,
-        required : [true, "Username is required"],
-        unique :[true, "please choose another username"]    
-    },
-    dob : {
-        type: Date,
-        
-    },
-    role : {
-        type:RadioNodeList,
-
-    },
-    profileImage: {
-        type: String,
-        required: false
-    },
-    phoneNumber: {
-        type: String,
-        required: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-
+  name: {
+    type: String,
+    required: [true, "Name feild is required"],
+    maxlength: 100,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [true, "Email is already exists on our website"],
+  },
+  username: {
+    tyep: String,
+    required: [true, "Username is required"],
+    unique: [true, "please choose another username"],
+  },
+  dob: {
+    type: Date,
+  },
+  role: {
+    type: String,
+  },
+  profileImage: {
+    type: String,
+    required: false,
+  },
+  phoneNumber: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
+UserSchema.pre("save", function (next) {
+  var user = this;
 
-UserSchema.pre('save', function(next) {
-    var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(SALT, function (err, salt) {
+      if (err) return next(err);
 
-    if(user.isModified('password')) {
-        bcrypt.genSalt(SALT, function(err, salt) {
-            if(err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
 
-            bcrypt.hash(user.password, salt, function(err, hash) {
-                if(err) return next(err);
+        user.password = hash;
 
-                user.password = hash;
-
-                next();
-            })
-        })
-    } else {
         next();
-    }
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 UserSchema.methods.comparePassword = function (userPassword, callBack) {
-    bcrypt.compare(userPassword, this.password, function(err, isMatch) {
-        if(err) {
-            return callBack(err);
-        }
+  bcrypt.compare(userPassword, this.password, function (err, isMatch) {
+    if (err) {
+      return callBack(err);
+    }
 
-        callBack(null, isMatch);
-    });
-}
+    callBack(null, isMatch);
+  });
+};
 
-UserSchema.methods.generateToken = function(callBack) {
-    var user = this;
+UserSchema.methods.generateToken = function (callBack) {
+  var user = this;
 
-    var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
 
-    callBack(null, token);
-}
+  callBack(null, token);
+};
 
-UserSchema.statics.findByToken = function(token, callBack) {
-    jwt.verify(token, process.env.SECRET, function(err, decode) {
-        User.findById(decode).then((user) => {
-            callBack(null, user);
-        }).catch((err) => {
-            callBack(null, err);
-        });
-    });
-}
+UserSchema.statics.findByToken = function (token, callBack) {
+  jwt.verify(token, process.env.SECRET, function (err, decode) {
+    User.findById(decode)
+      .then((user) => {
+        callBack(null, user);
+      })
+      .catch((err) => {
+        callBack(null, err);
+      });
+  });
+};
 
 const User = mongoose.model("User", UserSchema);
-module.exports = { User }
+module.exports = { User };
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = require("express") ();
+const app = require("express")();
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
@@ -9,10 +9,10 @@ app.use(cors());
 var port = process.env.PORT || 6000;
 
 mongoose.connect(process.env.DATABASE).then(() => {
-    console.log("Connected To Database");
-})
+  console.log("Connected To Database");
+});
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
@@ -20,11 +20,10 @@ var v1 = require("./routes/Index");
 
 app.use("/api/v1", v1.router);
 
-app.use(function(req, res) {
-    
-    res.status(404).send({url: req.originalUrl + ' not found'})
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
 app.listen(port, () => {
-    console.log("Pharmacist API started on port " + port);
-})
+  console.log("Pharmacist API started on port " + port);
+});
